5c77bdfc1412dc02ce0fa3f16eb297b716b44912
Clio <clio.f321@gmail.com>
2024-09-21 23:37:31 +0200

    job script for batch

---
+++ job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=llm-embedding
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=06:00:00
+#SBATCH --output=slurm_output_%A.out
+
+module load 2022 
+module load Anaconda3/2022.05 
+module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0 
+
+pip install --user transformers
+pip install --user accelerate
+pip install --user scikit-learn
+pip install --user datasets
+pip install --user pandas
+pip install --user 'seacrowd>=0.2.0'
+
+# Run your code
+srun python main.py
