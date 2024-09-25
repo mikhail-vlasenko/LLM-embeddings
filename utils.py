947436f6fc993538ec1f2088da8a87298293c4d8
Clio Feng <clio.f321@gmail.com>
2024-09-25 12:44:14 +0200

    update save embedding function

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
-import csv
 
+import pickle
 def save_embeddings(embeddings_dict, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(embeddings_dict, f)
+
+
+def load_embeddings(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+    
+
+
+
+    
     
     # Get all the unique "target_language-language" pairs as columns
     columns = []
