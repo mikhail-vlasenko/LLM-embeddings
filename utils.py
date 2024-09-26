4a6f89d65f994c7a0db37d2572cf2b7434491802
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2024-09-26 13:27:30 +0200

    10 languages supported and code improvements

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pickle
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def save_embeddings(embeddings_dict, filename):
     with open(filename, 'wb') as f:
@@ -9,3 +11,10 @@ def save_embeddings(embeddings_dict, filename):
 def load_embeddings(filename):
     with open(filename, 'rb') as f:
         return pickle.load(f)
+
+
+def plot_heatmap(data, title):
+    plt.figure(figsize=(12, 10), dpi=160)
+    sns.heatmap(data, cmap='Blues', annot=True, fmt=".2f")
+    plt.title(title)
+    plt.show()
